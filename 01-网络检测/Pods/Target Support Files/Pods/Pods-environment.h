62928e7db3275039a555ef567a30ba882462e138
Fengtf <1005052145@qq.com>
2015-07-13 10:17:44 +0800

    初始化

--- 01-网络检测/Pods/Target Support Files/Pods/Pods-environment.h
+++ 01-网络检测/Pods/Target Support Files/Pods/Pods-environment.h
@@ -48,3 +48,9 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 5
 #define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 4
 
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 1
+
