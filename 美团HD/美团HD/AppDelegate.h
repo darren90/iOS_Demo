03f3ee825f9d460e5fe30e7ac07876e81bf40fb0
darren90 <1005052145@qq.com>
2016-01-07 22:38:40 +0800

    add mt

---
+++ 美团HD/美团HD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  美团HD
+//
+//  Created by Tengfei on 16/1/7.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 美团HD/美团HD/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  美团HD
+//
+//  Created by Tengfei on 16/1/7.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UICollectionViewController
+
+@end
