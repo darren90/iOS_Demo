53564db60dd470fa5bb2268f9c18b51ba8213cac
Fengtf <1005052145@qq.com>
2016-01-08 16:56:56 +0800

    add OrientationDemo

---
+++ OrientationDemo/OrientationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrientationDemo
+//
+//  Created by Fengtf on 16/1/8.
+//  Copyright © 2016年 ftf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
