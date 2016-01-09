4a726b9c031c89c5924f0922c4fe0ec8292a637c
darren90 <1005052145@qq.com>
2016-01-09 22:33:31 +0800

    add district

--- 美团HD/美团HD/Class/Home/Controller/DistrictViewController.m
+++ 美团HD/美团HD/Class/Home/Controller/DistrictViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "DistrictViewController.h"
+#import "TFHomeDropdown.h"
+#import "Masonry.h"
 
 @interface DistrictViewController ()
+@property (weak, nonatomic) IBOutlet UIView *titleView;
 
 @end
 
@@ -17,6 +20,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    TFHomeDropdown *dropdown = [TFHomeDropdown dropDown];
+    [self.view addSubview:dropdown];
+//    dropdown.frame = CGRectMake(0, 44, self.view.frame.size.width, self.view.frame.size.height);
+    [dropdown mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.titleView.mas_bottom);
+        make.bottom.equalTo(self.view.mas_bottom);
+        make.left.equalTo(self.view.mas_left);
+        make.right.equalTo(self.view.mas_right);
+    }];
+//    dropdown.backgroundColor = [UIColor grayColor];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +38,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
