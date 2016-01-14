ee64f0b29457ef883e4e8edbaaf5e453ca4bb3ba
Fengtf <1005052145@qq.com>
2016-01-14 15:06:35 +0800

    init VKVideoPlayerPro

---
+++ VKVideoPlayerPro/VKVideoPlayerPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKVideoPlayerPro
+//
+//  Created by Fengtf on 16/1/14.
+//  Copyright © 2016年 ftf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
