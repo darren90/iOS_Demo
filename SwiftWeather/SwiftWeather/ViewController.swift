0531d15149ac4f8cd573d3559b688a2d34931c99
darren90 <1005052145@qq.com>
2016-01-24 22:52:23 +0800

    init swift weather

---
+++ SwiftWeather/SwiftWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWeather
+//
+//  Created by Tengfei on 16/1/24.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
