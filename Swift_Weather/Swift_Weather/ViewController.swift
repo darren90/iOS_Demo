c06569e6a29d55fe0b5128e55e6407a05f53c8fb
darren90 <1005052145@qq.com>
2016-01-25 22:16:16 +0800

    获取地理位置信息

--- Swift_Weather/Swift_Weather/ViewController.swift
+++ Swift_Weather/Swift_Weather/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
     let locationManger:CLLocationManager = CLLocationManager()
 
     override func viewDidLoad() {
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         locationManger.desiredAccuracy = kCLLocationAccuracyBest
-        
+        locationManger.delegate = self
         if(iOS8()){
             locationManger.requestAlwaysAuthorization()
         }
@@ -29,6 +29,22 @@ class ViewController: UIViewController {
         let versionDou:Double = Double(versionStr)!
         return versionDou > 8.0
     }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var location:CLLocation = locations[locations.count - 1] as CLLocation
+        if(location.horizontalAccuracy > 0){
+            print(location.coordinate.latitude)
+            print(location.coordinate.longitude)
+
+            locationManger.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error\(error)");
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
