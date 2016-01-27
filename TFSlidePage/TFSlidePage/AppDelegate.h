35fd259df76a7d0243ddeefd02e7957ab15f2b80
Fengtf <1005052145@qq.com>
2016-01-27 17:38:54 +0800

    init TFSlidePage

---
+++ TFSlidePage/TFSlidePage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TFSlidePage
+//
+//  Created by Fengtf on 16/1/27.
+//  Copyright © 2016年 ftf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TFSlidePage/TFSlidePage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TFSlidePage
+//
+//  Created by Fengtf on 16/1/27.
+//  Copyright © 2016年 ftf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
