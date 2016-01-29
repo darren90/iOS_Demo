4d345a6ddaba073094662599fbb5ee15c3e06362
darren90 <1005052145@qq.com>
2016-01-29 22:14:15 +0800

    add TFSlidePage

---
+++ TFSlidePage/TFSlidePage/TFSlidePage/TFSlidePageView.h
@@ -0,0 +1,13 @@
+//
+//  TFSlidePageView.h
+//  TFSlidePage
+//
+//  Created by Tengfei on 16/1/29.
+//  Copyright © 2016年 ftf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFSlidePageView : UIView
+
+@end
---
+++ TFSlidePage/TFSlidePage/TFSlidePage/TFSlidePageView.m
@@ -0,0 +1,21 @@
+//
+//  TFSlidePageView.m
+//  TFSlidePage
+//
+//  Created by Tengfei on 16/1/29.
+//  Copyright © 2016年 ftf. All rights reserved.
+//
+
+#import "TFSlidePageView.h"
+
+@implementation TFSlidePageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
