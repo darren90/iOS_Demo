b0bce76e14f0878bb3cd0a0ef344fcb81c802bd3
Fengtf <1005052145@qq.com>
2016-01-30 16:18:34 +0800

    modify

--- TFSlidePage/TFSlidePage/TFSlidePage/TFSlidePageView.m
+++ TFSlidePage/TFSlidePage/TFSlidePage/TFSlidePageView.m
@@ -8,14 +8,13 @@
 
 #import "TFSlidePageView.h"
 
+
+@interface TFSlidePageView()
+
+@end
+
 @implementation TFSlidePageView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+
 
 @end
