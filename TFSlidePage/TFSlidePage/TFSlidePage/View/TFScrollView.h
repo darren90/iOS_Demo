eb74b9fd0f935e73c4d11d3fc61893680e79ceb6
Fengtf <1005052145@qq.com>
2016-02-02 10:55:19 +0800

    add UIScrollView

---
+++ TFSlidePage/TFSlidePage/TFSlidePage/View/TFScrollView.h
@@ -0,0 +1,13 @@
+//
+//  TFScrollView.h
+//  TFSlidePage
+//
+//  Created by Fengtf on 16/2/2.
+//  Copyright © 2016年 ftf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFScrollView : UIScrollView
+
+@end
---
+++ TFSlidePage/TFSlidePage/TFSlidePage/View/TFScrollView.m
@@ -0,0 +1,21 @@
+//
+//  TFScrollView.m
+//  TFSlidePage
+//
+//  Created by Fengtf on 16/2/2.
+//  Copyright © 2016年 ftf. All rights reserved.
+//
+
+#import "TFScrollView.h"
+
+@implementation TFScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
