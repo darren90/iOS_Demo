6ed4145a8887950e0be59b3ed49f3d96a96d15c1
Fengtf <1005052145@qq.com>
2016-02-24 17:58:59 +0800

    modify something about web view

--- FileMaster/FileMaster/ViewController.swift
+++ FileMaster/FileMaster/ViewController.swift
@@ -20,6 +20,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    func readeFile(sender: UIButton) {
+        let path = ( NSHomeDirectory() as NSString ).stringByAppendingPathComponent("Documents")
+        let fm = NSFileManager.defaultManager()
+        
+        var array:[String] = []
+        do {
+            array = try fm.contentsOfDirectoryAtPath(path)
+        }catch {
+            print(error)
+        }
+        for name in array {
+            print("name:\(name),,,path:\(path.stringByAppendingString("/\(name)"))")
+        }
+    }
+    
+    func writeFile(sender: UIButton) {
+        let path = ( NSHomeDirectory() as NSString ).stringByAppendingPathComponent("Documents")
+        let fm = NSFileManager.defaultManager()
+        let fileName = path.stringByAppendingString("/rrr.png")
+        
+        let image = createImage()
+        let data = UIImagePNGRepresentation(image)
+        fm.createFileAtPath(fileName, contents: data, attributes: nil)
+    }
+    
+    func createImage() -> UIImage{
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 300, height: 320), false, 0.0)
+        let ctx = UIGraphicsGetCurrentContext()
+        let imageRect = CGRect(x: 0.0, y: 0.0, width: 300, height: 320)
+        //        UIColor.brownColor().setFill()
+        CGContextFillRect(ctx, imageRect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+
 
 }
 
