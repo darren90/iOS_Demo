fe7176b1e6a47c21881e40bf8e923a27d736232c
darren90 <1005052145@qq.com>
2016-02-28 14:42:00 +0800

    add ZhiZhuPlus2.0

---
+++ ZhiHuPlus2.0/ZhiHuPlus2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZhiHuPlus2.0
+//
+//  Created by Tengfei on 16/2/28.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
