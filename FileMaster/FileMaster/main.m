0ce2435ee8fd961e2db42e8babae56b98e74e091
darren90 <1005052145@qq.com>
2016-02-28 21:14:53 +0800

    add FileMaster OC

---
+++ FileMaster/FileMaster/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FileMaster
+//
+//  Created by Tengfei on 16/2/28.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
