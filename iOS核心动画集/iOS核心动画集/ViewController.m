8c0294817c14c53d70d3d2c78f93981f4588367a
Fengtf <1005052145@qq.com>
2016-03-18 21:16:15 +0800

    some Thing

--- iOS核心动画集/iOS核心动画集/ViewController.m
+++ iOS核心动画集/iOS核心动画集/ViewController.m
@@ -21,20 +21,13 @@
 }
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    //    CAKeyframeAnimation //按照轨迹走
-    
     CAAnimationGroup *group = [CAAnimationGroup animation];
-    //    group.repeatCount = MAXFLOAT;
+    group.repeatCount = MAXFLOAT;
     group.duration = 3;
     
     CABasicAnimation *ratation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.x"];
-    //    ratation.keyPath = @"transform";
-    //    CATransform3D t3d = CATransform3DMakeRotation(M_PI*MAXFLOAT, 1, 0, 0);
-    //    ratation.toValue = [NSValue valueWithCATransform3D:t3d];
-    //    ratation.repeatCount = MAXFLOAT;
-    //    ratation.duration = MAXFLOAT;
-    ratation.toValue = [NSNumber numberWithFloat: M_PI * 2.0*2000000 ];//倍数放大，一直可以转
-    ratation.duration = 2000000;
+    ratation.toValue = [NSNumber numberWithFloat: M_PI * 10.0*20000000 ];//倍数放大，一直可以转
+    ratation.duration = 20000000;
     ratation.cumulative = YES;
     
     CABasicAnimation *position = [CABasicAnimation animation];
@@ -43,6 +36,7 @@
     position.toValue = [NSValue valueWithCGPoint:CGPointMake(self.imageView.layer.position.x, self.imageView.layer.position.y - 300)]; // 终了帧
     
     group.animations = @[ratation,position];
+//    group.
     
     [self.imageView.layer addAnimation:group forKey:nil];
 }
