c6838201c602dbd31bcac18b9dc1588f61dd8a17
darren90 <1005052145@qq.com>
2016-03-21 21:27:18 +0800

    remove file

--- FileMaster_Swfit/Pods/Target Support Files/FXImageView/FXImageView-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "FXImageView.h"
-#import "UIImage+FX.h"
-
-FOUNDATION_EXPORT double FXImageViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char FXImageViewVersionString[];
-
